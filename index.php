67c2ca355313025b1e14c83a47d29435e7b4346a
Ajeya Paul <paulajeya@gmail.com>
2020-05-15 21:15:35 +0530

    Added Header and Footer

--- index.php
+++ index.php
@@ -1,11 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title></title>
-</head>
-<body>
-    
-</body>
-</html>
+<?php get_header();?>
+<div id="primary" class="content-area">
+
+</div>
+<?php get_footer();?>
